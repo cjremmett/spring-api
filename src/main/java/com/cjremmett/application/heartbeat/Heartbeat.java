0db3d260be744f10c30410bc7719e2c055e742e3
boggledstarsector <boggledstarsector@gmail.com>
2025-04-09 16:28:04 -0400

    hello world api endpoint

---
+++ src/main/java/com/cjremmett/application/heartbeat/Heartbeat.java
@@ -0,0 +1,34 @@
+package com.cjremmett.application.heartbeat;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+public class Heartbeat {
+
+	public class HeartbeatReport
+	{
+		public HeartbeatReport()
+		{
+			Date date = new Date();
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			timestamp = dateFormat.format(date);
+		}
+
+		private String timestamp;
+
+		public String getTimestamp()
+		{
+			return timestamp;
+		}
+	}
+
+	@GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+	public HeartbeatReport getHeartbeat()
+	{
+		return new HeartbeatReport();
+	}
+}
