9698b864e6ebbe1b0300955034d1dd3860a5905a
boggledstarsector <boggledstarsector@gmail.com>
2025-04-10 10:50:41 -0400

    set up context path

--- src/main/java/com/cjremmett/application/Application.java
+++ src/main/java/com/cjremmett/application/Application.java
@@ -7,12 +7,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	ForwardedHeaderFilter forwardedHeaderFilter() {
+		return new ForwardedHeaderFilter();
 	}
 
 	@Bean
